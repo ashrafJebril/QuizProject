f42c79652ae7fa47f0eea872e9bbd373a24bd682
ashraf <ashrafjebril9@gmail.com>
2018-10-04 11:01:30 +0300

    last version fixed

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
   var questions = [
-    "Who was the first president of the USA?",
+    "whom you will love the most in the immersive?",
     "How many oscars did the Titanic movie got?",
     "question3",
     "question4",
-    "question5",
-    "question6",
-    "question7",
-    "question8",
-    "question9",
-    "question10"
+    "question5"
   ];
   var answers = {
-    "Who was the first president of the USA?": [
-      "Thomas Jefferson",
-      "George Washington",
-      "Abraham Lincoln",
-      "John Adams"
+    "whom you will love the most in the immersive?": [
+      "Your pair",
+      "Ahmad",
+      "HIR's",
+      "Sanaa"
     ],
     "How many oscars did the Titanic movie got?": [
       "Eight",
@@ -41,43 +36,16 @@ $(document).ready(function() {
       "question5 answer2",
       "question5 answer3",
       "question5 answer4"
-    ],
-    question6: [
-      "question6 answer1",
-      "question6 answer2",
-      "question6 answer3",
-      "question6 answer4"
-    ],
-    question7: [
-      "question7 answer1",
-      "question7 answer2",
-      "question7 answer3",
-      "question7 answer4"
-    ],
-    question8: [
-      "question8 answer1",
-      "question8 answer2",
-      "question8 answer3",
-      "question8 answer4"
-    ],
-    question9: [
-      "question9 answer1",
-      "question9 answer2",
-      "question9 answer3",
-      "question9 answer4"
-    ],
-    question10: [
-      "question10 answer1",
-      "question10 answer2",
-      "question10 answer3",
-      "question10 answer4"
     ]
   };
   var corectAnswers = {
     "Who was the first president of the USA?": "George Washington",
     "How many oscars did the Titanic movie got?": "Eleven",
-    question3: "question3 answer1"
+    question3: "question3 answer1",
+    question4: "question4 answer2",
+    question5: "question5 answer3"
   };
+  var selectedAnswers = {};
   var score = 0;
   var i = 0;
   var isAnswerd = false;
@@ -124,16 +92,50 @@ $(document).ready(function() {
           $(id).click(answerButtonsClicked);
         });
       } else {
-        $(".box").html("");
+        $("body").html("");
         //TODO: style this in the
-        $(".box").append("Your Score is " + score);
+        var index = 0;
+        var selectedIndex = 10;
+        $("body").append('<div id="lastPage"></div>');
+        $("#lastPage").append("<div class='score'>Your Score is " + score+"</div>");
+
+        for (var key in answers) {
+          console.log(answers[key]);
+          $("#lastPage").append("<br>");
+          $("#lastPage").append(key);
+          $("#lastPage").append("<br>");
+          $("#lastPage").append(
+            "<button  class='lastButtonAswers' id=" +
+              index +
+              ">" +
+              corectAnswers[key] +
+              "</button>"
+          );
+          $("#" + index).css("background-color", "green");
+        //  $("#lastPage").append("<br>");
+          if (corectAnswers[key] !== selectedAnswers[key]) {
+            $("#lastPage").append(
+              "<button class='lastButtonAswers' id=" +
+                selectedIndex +
+                ">" +
+                selectedAnswers[key] +
+                "</button>"
+            );
+
+            $("#" + selectedIndex).css("background-color", "red");
+          }
+          selectedIndex++;
+          index++;
+        }
       }
     } else {
       alert("Please select answer!");
     }
   }
-   function answerButtonsClicked() {
+
+  function answerButtonsClicked() {
     if (!isAnswerd) {
+      selectedAnswers[question] = $(this).text();
       $(this).css("background-color", "#3e8e41");
       if (corectAnswers[question] === answers[question][this.id]) {
         score++;
